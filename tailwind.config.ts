f05deb29fa217272ca2e8c75d07bc4c5cfafd18b
ier24 <ryohr93@gmail.com>
2023-08-20 15:33:57 +0900

    :chore: tailwind.config.tsをコマンドで生成

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
-const config: Config = {
+/**
+ * https://tailwindcss.com/docs/configuration
+ * npx tailwindcss init --tsで生成
+ */
+export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontFamily: {
-      sans: ["Noto Sans JP", "font-sans"],
-    },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
-};
-export default config;
+  plugins: [],
+} satisfies Config
+
+// 変数割当が必要か分からないためいったん上記の形で記述している。
+
+// const config: Config = {
+//   content: [
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     fontFamily: {
+//       sans: ["Noto Sans JP", "font-sans"],
+//     },
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//     },
+//   },
+//   plugins: [require("tailwindcss"), require("autoprefixer")],
+// };
+// export default config;
