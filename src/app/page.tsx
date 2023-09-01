359c923c12167789e5f474f6271e8a4d6561987a
ier24 <ryohr93@gmail.com>
2023-09-02 08:15:08 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,52 +6,8 @@ export default function Page() {
   const router = useRouter();
 
   return (
-    <div>
-      <h1 className="text-base font-bold leading-7 text-gray-900">
-        This is Root Page!
-      </h1>
-      <button type="button" onClick={() => router.push("/")}>
-        Homeに戻る
-      </button>
-      <br />
-      <button type="button" onClick={() => router.push("/posts")}>
-        投稿ページへ
-      </button>
-      <br />
-      <button type="button" onClick={() => router.push("/profile")}>
-        Profileページへ
-      </button>
-      <br />
-      <button type="button" onClick={() => router.push("/profil")}>
-        Not Found
-      </button>
-      <br />
-      <div className="">
-        <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
-          Badge
-        </span>
-        <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-          Badge
-        </span>
-        <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
-          Badge
-        </span>
-        <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-          Badge
-        </span>
-        <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-          Badge
-        </span>
-        <span className="inline-flex items-center rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10">
-          Badge
-        </span>
-        <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
-          Badge
-        </span>
-        <span className="inline-flex items-center rounded-md bg-pink-50 px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">
-          Badge
-        </span>
-      </div>
+    <div className="container mx-auto">
+      <h1>This is Root Page!</h1>
     </div>
   );
 }
